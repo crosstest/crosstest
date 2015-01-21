43bf3d5286865f4b09f4c2e64023aeabce5a46fb
Max Lincoln <max@devopsy.com>
2015-01-21 15:56:11 -0500

    Update windows bootstrap script

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,4 +1,7 @@
 bundle install
-# Replace sh files w/ ps1 files... they should be platform independent except
-# for the file extension.
-Dir samples/*.sh | Rename-Item -NewName {  $_.name  -replace '\.sh$','.ps1'  }
+pushd samples
+  # Replace sh files w/ ps1 files... they should be platform independent except
+  # for the file extension.
+  Dir *.sh | Rename-Item -NewName {  $_.name  -replace '\.sh$','.ps1'  }
+  bundle exec crosstest clone
+popd
