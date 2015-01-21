2f08d05f3617d02c08ecb3851c2b089dd35c6971
Max Lincoln <max@devopsy.com>
2015-01-21 15:59:56 -0500

    Rubocop

--- samples/tests/crosstest/validators.rb
+++ samples/tests/crosstest/validators.rb
@@ -1,7 +1,7 @@
 require 'crosstest'
 
 Crosstest.validate 'Hello world validator', suite: 'Katas', scenario: 'hello world' do |scenario|
-  expect(scenario.result.stdout.strip).to match "Hello, world!"
+  expect(scenario.result.stdout.strip).to match 'Hello, world!'
 end
 
 Crosstest.validate 'Quine output matches source code', suite: 'Katas', scenario: 'quine' do |scenario|
@@ -19,5 +19,5 @@ Crosstest.validate 'default validator' do |scenario|
   stderr = scenario.result.stderr
   stderr.gsub!(/DL is deprecated, please use Fiddle[\r\n]+/, '') # Known windows warning
   expect(stderr).to be_empty
-  expect(scenario.result.stdout).to end_with /$/
+  expect(scenario.result.stdout).to end_with(/$/)
 end
